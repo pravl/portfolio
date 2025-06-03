74f2390655947b8a2d8513aeec43afccdc35b203
Praval Jain <praval41195jain@gmail.com>
2025-06-03 16:52:03 +0530

    fix background color

--- src/App.tsx
+++ src/App.tsx
@@ -9,20 +9,34 @@ import './styles/main.css';
 
 function App() {
   return (
-    <Router>
-      <ThemeProvider>
-        <div className="app">
-          <Navbar />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/blog" element={<Blog />} />
-            <Route path="/projects" element={<Projects />} />
-            <Route path="/contact" element={<Contact />} />
-          </Routes>
-        </div>
-      </ThemeProvider>
-    </Router>
+    <>
+      <div className="background-mesh"></div>
+      <div className="particles">
+        <div className="particle"></div>
+        <div className="particle"></div>
+        <div className="particle"></div>
+        <div className="particle"></div>
+        <div className="particle"></div>
+        <div className="particle"></div>
+        <div className="particle"></div>
+        <div className="particle"></div>
+        <div className="particle"></div>
+      </div>
+      <Router>
+        <ThemeProvider>
+          <div className="app">
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/blog" element={<Blog />} />
+              <Route path="/projects" element={<Projects />} />
+              <Route path="/contact" element={<Contact />} />
+            </Routes>
+          </div>
+        </ThemeProvider>
+      </Router>
+    </>
   )
 }
 
-export default App 
+export default App
